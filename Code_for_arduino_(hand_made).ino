0f18608c58c4cae738d690c6d0701c7afcfd4e35
Dev-xuT <dtmaster@proton.me>
2026-05-07 15:03:43 -0300

    Update PWM pin and LCD connections in Arduino code
    Set 31khz frequency on pin 9

--- Code_for_arduino_(hand_made).ino
+++ Code_for_arduino_(hand_made).ino
@@ -1,15 +1,15 @@
 #include <LiquidCrystal.h>
 
-const int pwmpin = 10;
-const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int pwmpin = 9;
+const int rs = 13, en = 12, d4 = 8, d5 = 7, d6 = 4, d7 = 2; // LCD conection exclude the PWM capable pins
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int pwmout = 0;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  //Start fans in full speed in case of erros
-  analogWrite(pwmpin, 255);
+  TCCR1B = (TCCR1B & 0b11111000) | 0x01; // Enable 31 khz PWM output 
+  analogWrite(pwmpin, 0);
 }
 
 void loop() {
