8bcd4621afda77d0230dd0c5b5a6d0e335d70740
Dev-xuT <dtmaster@proton.me>
2026-05-08 11:14:48 -0300

    Refactor PWM pins and update LCD messages

--- Code_for_arduino_(hand_made).ino
+++ Code_for_arduino_(hand_made).ino
@@ -1,28 +1,39 @@
 #include <LiquidCrystal.h>
 
-const int pwmpin = 9;
-const int rs = 13, en = 12, d4 = 8, d5 = 7, d6 = 4, d7 = 2; // LCD conection exclude the PWM capable pins
+const int pwmCPUpin = 9;
+const int pwmCHApin = 10;
+const int rs = 13, en = 12, d4 = 8, d5 = 7, d6 = 4, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int pwmout = 0;
+int pwmCPUout = 0;
+int pwmCHAout = 0;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
   TCCR1B = (TCCR1B & 0b11111000) | 0x01; // Enable 31 khz PWM output 
-  analogWrite(pwmpin, 0);
+  analogWrite(pwmCPUpin, 200);  // Set minimal speed for system boot
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+  lcd.setCursor(0, 1);
+  delay(1000);
+  lcd.print("Not Conected!!!");
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String temp = Serial.readStringUntil('\n');
     String usage = Serial.readStringUntil('\n');
-    String pwm = Serial.readStringUntil('\n');
+    String pwmCPU = Serial.readStringUntil('\n');
+    String pwmCHA = Serial.readStringUntil('\n');
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(temp);
     lcd.setCursor(0, 1);
     lcd.print(usage);
-    sscanf(pwm.c_str(), "pwm = %d", &pwmout);
-    analogWrite (pwmpin, pwmout);
+    sscanf(pwmCPU.c_str(), "pwmCPU = %d", &pwmCPUout);
+    sscanf(pwmCHA.c_str(), "pwmCHA = %d", &pwmCHAout);
+    analogWrite(pwmCPUpin, pwmCPUout);
+    analogWrite(pwmCHApin, pwmCHAout);
   }    
 }
