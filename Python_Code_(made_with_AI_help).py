54b72b66bf19990df5660509380d850ea86aa4ea
Dev-xuT <dtmaster@proton.me>
2026-05-08 14:59:05 -0300

    Update comment for clarity on screen clearing

--- Python_Code_(made_with_AI_help).py
+++ Python_Code_(made_with_AI_help).py
@@ -10,7 +10,7 @@ def map_value(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 while True:
-    # Clear screen using Escape ANSI
+    # Clear screen with Escape ANSI
     print('\033[2J\033[H', end='', flush=True)
     
     # Get temperatures
