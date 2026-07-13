cbde06d95ea977d4564675215545fdaf558c34a5
Dev-xuT <dtmaster@proton.me>
2026-07-13 09:35:25 -0300

    Update fan control logic in Arduino code
    Refactor fan speed control logic for CPU and case fans based on temperature thresholds.

--- Code_for_Arduino_no_Display(Handmade).ino
+++ Code_for_Arduino_no_Display(Handmade).ino
@@ -1,16 +1,17 @@
-#include <LiquidCrystal.h>
-
 const int pwmCPUpin = 9;
 const int pwmCASpin = 10;
+
 int pwmCPUout = 0;
 int pwmCASout = 0;
+int tempsts = 0;
+int CASrest = 133;
+int CPUrest = 105;
 
 void setup() {
   Serial.begin(9600);
   TCCR1B = (TCCR1B & 0b11111000) | 0x01; // Enable 31 khz PWM output. Only for pins 9 & 10
-  analogWrite(pwmCPUpin, 172); // 50% CPU fan speed in boot
-  analogWrite(pwmCASpin, 90); // 20% CASe fan speed in boot
-  delay(1000);
+  analogWrite(pwmCPUpin, 162); // 50% CPU fan speed in boot
+  analogWrite(pwmCASpin, 0); // 20% CASe fan speed in boot
 }
 
 void loop() {
@@ -19,7 +20,19 @@ void loop() {
     String pwmCAS = Serial.readStringUntil('\n');
     sscanf(pwmCPU.c_str(), "pwmCPU = %d", &pwmCPUout);
     sscanf(pwmCAS.c_str(), "pwmCAS = %d", &pwmCASout);
-    analogWrite(pwmCPUpin, pwmCPUout);
-    analogWrite(pwmCASpin, pwmCASout);
+    if (pwmCPUout >= 105) {
+      analogWrite(pwmCPUpin, pwmCPUout);
+    } else {
+      analogWrite(pwmCPUpin, CPUrest);
+    }
+    if (pwmCASout >= 133) {
+      analogWrite(pwmCASpin, pwmCASout);
+      tempsts = 1;
+    } else if (pwmCASout <= 51) {
+      analogWrite(pwmCASpin, 0);
+      tempsts = 0;
+    } else if (pwmCASout >= 52 <= 132 && tempsts == 1) {
+      analogWrite(pwmCASpin, CASrest);
+    }
   }
 }
